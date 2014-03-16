2d1e729ddd9b065f43c0981b2157e10d25e58380
Sam Carr <samcarr@me.com>
2014-03-16 22:29:37 +0000

    Improve skeleton, with Try based error handling.
    File reading is simple but hopefully effective enough.

--- src/main/samcarr/tp2wp/Main.scala
+++ src/main/samcarr/tp2wp/Main.scala
@@ -2,29 +2,60 @@ package samcarr.tp2wp;
 
 import IoUtils._
 import java.io.File
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
+import scala.io.Source
 
 object Main {
     
     implicit val FileEncoding = Utf8
     
     def main(args: Array[String]) {
-        parseArgs(args) { inputFilePath =>
-             
+        migrate(args) match {
+            case Success(_) => println("Completed")
+            case Failure(e) => println(e.getMessage())
         }
     }
     
-    private def parseArgs(args: Array[String])(act:String => Unit) {
-        if (args.length < 1) {
-            println("Usage: tp2wp <typepad_export_file>")
+    private def migrate(args: Array[String]) = {
+        for {
+            config <- parseArgs(args)
+            contents <- readInput(config)
+            converted <- convertImageUrls(contents)
+            result <- writeOutput(converted, config)
+        } yield result
+    }
+    
+    private def parseArgs(args: Array[String]): Try[Config] = {
+        if (args.length < 2) {
+            Failure(new IllegalArgumentException("Usage: tp2wp <typepad_export_file> <output_dir>"))
         } else {
-            act(args(0))
+            Success(Config(args(0), args(1)))
         }
     }
     
-    private def readInput(filePath: String): Option[String] = {
-        withSource(new File(filePath)) { source =>
-            Some("")
+    private def readInput(config: Config): Try[String] = {
+        Try {
+            // Reading the whole file and only closing when there are no errors is not great
+            // but it's quite sufficient for this one-shot command-line app.
+            val source = Source.fromFile(new File(config.inputFileName), FileEncoding.name)
+            val contents = source.getLines().mkString("\n")
+            source.close()
+            contents
         }
     }
     
+    private def convertImageUrls(contents: String): Try[String] = {
+        Success("")
+    }
+    
+    private def writeOutput(converted: String, config: Config): Try[Unit] = {
+        Try {
+            val dir = new File(config.outputDirName)
+            dir.mkdirs()
+        }
+    }
 }
+
+case class Config(inputFileName: String, outputDirName: String)
