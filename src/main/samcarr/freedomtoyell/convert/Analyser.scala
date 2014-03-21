2ebd2a8f5c5a02619c227f34b017838417d10691
Sam Carr <samcarr@me.com>
2014-03-21 23:53:32 +0000

    Add comment about potentially using triples instead of Map.

--- src/main/samcarr/freedomtoyell/convert/Analyser.scala
+++ src/main/samcarr/freedomtoyell/convert/Analyser.scala
@@ -6,6 +6,7 @@ import java.net.URI
 case class ImageUris(importUri: URI, finalUri: URI)
 
 object Analyser {
+    // qq Could return a set of triples (case class though) as we never actually look up by key.
     def analyse(content: String)(implicit config: Config): Map[URI, ImageUris] = {
         val uris = UriExtractor.extract(content)(config)
         // Duplicate URI keys will be coalesced (last one wins) due to the nature of a Map.
