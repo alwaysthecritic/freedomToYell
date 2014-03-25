f9dbddc6af8420d149a4459d49ea79723e3ca002
Sam Carr <samcarr@me.com>
2014-03-25 07:20:20 +0000

    Document usage more thoroughly in the usage message.

--- src/main/samcarr/freedomtoyell/Usage.scala
+++ src/main/samcarr/freedomtoyell/Usage.scala
@@ -4,5 +4,14 @@ object Usage {
     val Message =
 """#Usage: freedomToYell <tp_export> <old_host> <new_host> <tp_article_prefix> <output_dir>
    #For example:
-   #  freedomToYell export.txt www.old.com new.net blog/ migrated""".stripMargin('#')
+   #  freedomToYell export.txt www.old.com new.net blog/ migrated
+   #
+   #FreedomToYell is a tool to help migrate a Typepad blog to WordPress.
+   #Export your blog content from Typepad to get a file containing all the posts and comments.
+   #Run this tool, which takes that file <tp_export>, and writes out a new version to <output_dir>
+   #with article and image URIs corrected and all images downloaded. Import the new file with the
+   #standard WordPress Typepad importer and upload the images directory 'tp' to your wp-content
+   #directory on the server (probably via FTP).
+   #This assumes that you have setup WordPress with the same permalink format for posts."""
+        .stripMargin('#')
 }
