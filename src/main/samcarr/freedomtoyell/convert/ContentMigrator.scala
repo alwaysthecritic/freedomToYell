f3b535a1e96c2e41311880ebeb46cf66e8348827
Sam Carr <samcarr@me.com>
2014-04-27 21:30:08 +0100

    Improve names in ContentMigrator to match reality.

--- src/main/samcarr/freedomtoyell/convert/ContentMigrator.scala
+++ src/main/samcarr/freedomtoyell/convert/ContentMigrator.scala
@@ -3,9 +3,9 @@ package samcarr.freedomtoyell.convert
 import java.net.URI
 
 object ContentMigrator {
-    def migrate(content: String, uriMap: Set[MigratedUri]): String = {
+    def migrate(content: String, uris: Set[MigratedUri]): String = {
         var migratedContent = content
-        uriMap foreach { case MigratedUri(originalUri, _, finalUri) =>
+        uris foreach { case MigratedUri(originalUri, _, finalUri) =>
             migratedContent = migratedContent.replaceAllLiterally(originalUri.toString, finalUri.toString)
         }
         migratedContent
